e3d1b6331e66e7edaefc7c988132bc0ef2630e03
Spencer Pease <spencer.p.816@gmail.com>
2019-06-28 16:08:38 -0700

    Update code style

--- R/initializeLogging.R
+++ R/initializeLogging.R
@@ -1,61 +1,41 @@
+#' @title Initialize standard log files
+#'
+#' @description
+#' Convenience function that wraps logging initialization steps common to Mazama
+#' Science web services.
+#'
+#' @param logDir Directory in which to write log files.
+#' @return NULL
+#'
 #' @name initializeLogging
 #' @export
-#' @title Initialize standard log files
-#' @param logDir Directory in which to write log files.
-#' @return No return value.
-#' @description Convenience function that wraps logging initialization steps
-#' common to Mazama Science web services:
-#' 
-#' \preformatted{
-#' result <- try({
-#'   Copy and old log files
-#'   timestamp <- strftime(lubridate::now(), "%Y-%m-%dT%H:%M:%S")
-#'   for ( logLevel in c("TRACE","DEBUG","INFO","ERROR") ) {
-#'     oldFile <- file.path(logDir,paste0(logLevel,".log"))
-#'     newFile <- file.path(logDir,paste0(logLevel,".log.",timestamp))
-#'     if ( file.exists(oldFile) ) {
-#'       file.rename(oldFile, newFile)
-#'     }
-#'   }
-#' }, silent=TRUE)
-#' stopOnError(result, "Could not rename old log files.")
-#' 
-#' result <- try({
-#'   # Set up logging
-#'   logger.setup(traceLog = file.path(logDir, "TRACE.log"),
-#'                debugLog=file.path(logDir, "DEBUG.log"),
-#'                infoLog=file.path(logDir, "INFO.log"),
-#'                errorLog=file.path(logDir, "ERROR.log"))
-#' }, silent=TRUE)
-#' stopOnError(result, "Could not create log files.")
-#' }
-
+#'
 initializeLogging <- function(logDir= NULL) {
-  
-  if ( is.null(logDir) ) {
-    stop("Required parameter 'logDir' is missing.", call.=FALSE)
+
+  if (is.null(logDir)) {
+    stop("Required parameter 'logDir' is missing.", call. = FALSE)
   }
-  
+
   result <- try({
     # Copy and old log files
     timestamp <- strftime(lubridate::now(), "%Y-%m-%dT%H:%M:%S")
-    for ( logLevel in c("TRACE","DEBUG","INFO","ERROR") ) {
-      oldFile <- file.path(logDir,paste0(logLevel,".log"))
-      newFile <- file.path(logDir,paste0(logLevel,".log.",timestamp))
-      if ( file.exists(oldFile) ) {
+    for (logLevel in c("TRACE", "DEBUG", "INFO", "ERROR")) {
+      oldFile <- file.path(logDir, paste0(logLevel, ".log"))
+      newFile <- file.path(logDir, paste0(logLevel, ".log.", timestamp))
+      if (file.exists(oldFile)) {
         file.rename(oldFile, newFile)
       }
     }
-  }, silent=TRUE)
+  }, silent = TRUE)
   stopOnError(result, "Could not rename old log files.")
-  
+
   result <- try({
     # Set up logging
     logger.setup(traceLog = file.path(logDir, "TRACE.log"),
-                 debugLog=file.path(logDir, "DEBUG.log"),
-                 infoLog=file.path(logDir, "INFO.log"),
-                 errorLog=file.path(logDir, "ERROR.log"))
-  }, silent=TRUE)
+                 debugLog = file.path(logDir, "DEBUG.log"),
+                 infoLog = file.path(logDir, "INFO.log"),
+                 errorLog = file.path(logDir, "ERROR.log"))
+  }, silent = TRUE)
   stopOnError(result, "Could not create log files.")
-  
+
 }

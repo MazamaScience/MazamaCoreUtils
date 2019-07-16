89053425c73ad3728a71d65e5812ca4ddb361b71
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2019-07-16 10:46:20 -0700

    fixed bugs in dateRange()

--- R/utils-time.R
+++ R/utils-time.R
@@ -2,14 +2,16 @@
 #' 
 #' @title Create a POSIXct date range
 #' 
-#' @param startdate desired start datetime (ISO 8601)
-#' @param enddate desired end datetime (ISO 8601)
-#' @param days number of days of data to include
-#' @param timezone Olson timezone used to interpret dates
-#' @param unit units used to determine enddates final timestep
+#' @param startdate Desired start datetime (ISO 8601).
+#' @param enddate Desired end datetime (ISO 8601).
+#' @param days Number of days of data to include.
+#' @param timezone Olson timezone used to interpret dates.
+#' @param unit Units used to determine enddates final timestep.
 #' 
 #' @description Uses incoming parameters to return a pair of \code{POSIXct}
-#' times in the proper order. This function always rounds to day boundaries.
+#' times in the proper order. The first returned time will be midnight of
+#' the desired starting date. The second returned time will represent the
+#' "end of the day" of the requested or calcualted \code{enddate}.
 #' 
 #' Dates can be anything that is understood by 
 #' \code{lubrdiate::parse_date_time()} including either of the following 
@@ -20,10 +22,29 @@
 #' \item{\code{"YYYY-mm-dd"}}
 #' }
 #' 
-#' @note The ending \code{POSIXct} will end one \code{unit} before the specified
-#' \code{enddate}.
+#' @note The second of the returned \code{POSIXct}s will end one \code{unit} 
+#' before the specified \code{enddate}. Acceptable units are 
+#' \code{"day", "hour", "min", "sec"}.
+#' 
+#' The aim is to quickly calculate full-day date ranges for time series whose 
+#' values are binned at different units. Thus, if \code{unit = "min"}, the 
+#' returned value associated with \code{enddate} will always be at 23:59:00
+#' in the requested time zone.
+#' 
+#' @note Because the returned values always satisfy full-day date ranges, 
+#' specifying \code{unit = "day"} will cause the returned \code{enddate} to
+#' be associated with 00:00:00 of the following day.
 #' 
 #' @return A vector of two \code{POSIXct}s.
+#' 
+#' @examples
+#' dateRange("2019-01-08")
+#' dateRange("2019-01-08", unit = "min")
+#' dateRange("2019-01-08", unit = "hour")
+#' dateRange("2019-01-08", unit = "day")
+#' dateRange("2019-01-08", "2019-01-11")
+#' dateRange(enddate = 20190112, days = 3, 
+#'           unit = "day", timezone = "America/Los_Angeles")
 
 dateRange <- function(
   startdate = NULL, 
@@ -36,13 +57,13 @@ dateRange <- function(
   # ----- Determine starttime and endtime --------------------------------------
   
   if ( stringr::str_detect(unit, "^day") ) {
-    daySecs <- 0
-  } else if ( stringr::str_detect(unit, "^day") ) {
-    daySecs <- 60 * 60 * 23
+    daySecs <- 60 * 60 * 24
+  } else if ( stringr::str_detect(unit, "^hour") ) {
+    daySecs <- 60 * 60 * 24 - 3600
   } else if ( stringr::str_detect(unit, "^min") ){
-    daySecs <- 60 * 60 * 23 - (60 * 1)
+    daySecs <- 60 * 60 * 24 - 60
   } else if ( stringr::str_detect(unit, "^sec") ) {
-    daySecs <- 60 * 60 * 24 - (1)
+    daySecs <- 60 * 60 * 24 - 1
   }
   
   orders <- c("Ymd","YmdH","YmdHM","YmdHMS")
@@ -70,10 +91,15 @@ dateRange <- function(
       lubridate::floor_date(unit = "day") + 
       lubridate::dseconds(daySecs)               # end of day
     
-    starttime <- 
-      endtime %>%
-      lubridate::floor_date(unit = "day") -      # beginning of day
-      lubridate::ddays(days-1)                   # any extra days
+    # Special case for "day" because enddate moves to 00:00:00 of next day
+    if ( unit == "day" ) {
+      starttime <- endtime - lubridate::ddays(days)
+    } else {
+      starttime <- 
+        endtime %>%
+        lubridate::floor_date(unit = "day") -      # beginning of day
+        lubridate::ddays(days-1)                   # any extra days
+    }
     
   } else if ( !is.null(startdate) && is.null(enddate) ) {
     

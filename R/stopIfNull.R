a388ce3b6c9f63a5f62427df0228dbcbf7aea826
Spencer Pease <spencer.p.816@gmail.com>
2019-08-21 15:54:02 -0700

    Add function `stopIfNull()`
    References: #17

---
+++ R/stopIfNull.R
@@ -0,0 +1,52 @@
+#' Stop if an object is NULL
+#'
+#' @description
+#' This is a convenience function for testing if an object is \code{NULL}, and
+#' providing a custom error message if it is.
+#'
+#' @param target Object to test if \code{NULL}.
+#' @param msg Optional custom message to display when \code{target} is
+#'   \code{NULL}.
+#'
+#' @return If \code{target} is not \code{NULL}, \code{target} is returned
+#'   invisibly.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' # Return input invisibly if not NULL
+#' x <- stopIfNull(5, msg = "Custom message")
+#' print(x)
+#'
+#' \dontrun{
+#' testVar <- NULL
+#' stopIfNull(testVar)
+#' stopIfNull(testVar, msg = "This is NULL")
+#' }
+stopIfNull <- function(target, msg = NULL) {
+
+  # Return early if not NULL ---------------------------------------------------
+
+  if (!is.null(target))
+    return(invisible(target))
+
+
+  # Build error message --------------------------------------------------------
+
+  if (is.null(msg)) {
+
+    msg <- paste0("'", deparse(substitute(target)), "' must not be NULL.")
+
+  } else if (!is.character(msg) || length(msg) != 1) {
+
+    stop("msg must be a length one character string.")
+
+  }
+
+
+  # Stop -----------------------------------------------------------------------
+
+  stop(msg, call. = FALSE)
+
+}

31628056bbbd344471293fb67c1e6201eb08500e
Kayleigh Wilson <kayleigh@Unknown.domain>
2019-09-24 15:32:32 -0700

    created three functions to check additional tests and examples.

--- R/devtools_check.R
+++ R/devtools_check.R
@@ -48,7 +48,7 @@ check_faster <- function(
 
 #' @export
 #'
-#' @title Check without building vignettes or examples
+#' @title Check without building vignettes, examples or tests
 #'
 #' @description Runs devtools::check() with appropriate arguments to avoid
 #' documentation rebuild, vignettes, examples and tests.
@@ -66,6 +66,76 @@ check_fastest <- function(
     pkg = pkg,
     build_args = c('--no-build-vignettes'),
     args = c('--ignore-vignettes','--no-tests', '--no-examples', '--no-manual')
+  )
+
+}
+
+#' @export
+#'
+#' @title Check and run donttest{} examples
+#'
+#' @description Runs devtools::check() with appropriate arguments to run
+#' donttest{} examples
+#'
+#' @param pkg description, can be path or package name. See as.package() for
+#'  more information
+#'
+#' @return No return.
+
+check_slow <- function(
+  pkg = "."
+) {
+
+  devtools::check(
+    pkg = pkg,
+    args = c('--run-donttest')
+  )
+
+}
+
+#' @export
+#'
+#' @title Check and run donttest{} and dontrun{} examples
+#'
+#' @description Runs devtools::check() with appropriate arguments to run
+#' donttest{} and dontrun{} examples
+#'
+#' @param pkg description, can be path or package name. See as.package() for
+#'  more information
+#'
+#' @return No return.
+
+check_slower <- function(
+  pkg = "."
+) {
+
+  devtools::check(
+    pkg = pkg,
+    args = c('--run-donttest', '--run-dontrun')
+  )
+
+}
+
+#' @export
+#'
+#' @title Check and run donttest{} and dontrun{} examples, max testing
+#'
+#' @description Runs devtools::check() with appropriate arguments to run
+#' donttest{} and dontrun{} examples, maximum testing with things like
+#' --use-valgrind
+#'
+#' @param pkg description, can be path or package name. See as.package() for
+#'  more information
+#'
+#' @return No return.
+
+check_slowest <- function(
+  pkg = "."
+) {
+
+  devtools::check(
+    pkg = pkg,
+    args = c('--run-donttest', '--run-dontrun', '--use-valgrind')
   )
 
 }
